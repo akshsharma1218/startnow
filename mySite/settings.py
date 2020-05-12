26e80cc345fd43533dfdb0e3cc0b8f64f76ef4dc
AkshSharma <cnt2k20@gmail.com>
2020-05-12 15:24:57 +0530

    new name

--- mySite/settings.py
+++ mySite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get("debug_value") == 'True' )
 
-ALLOWED_HOSTS = ["letsdoitagain.herokuapp.com"]
+ALLOWED_HOSTS = ["startnow.herokuapp.com"]
 
 
 # Application definition
