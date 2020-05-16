c9e9dbc3f8303b207d701b55b41e1545526962e7
AkshSharma <cnt2k20@gmail.com>
2020-05-16 17:01:09 +0530

    shellphish

--- mySite/settings.py
+++ mySite/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = "False"
+DEBUG = (os.environ.get('debug_value')=='True')
 
 ALLOWED_HOSTS = ["startnow.herokuapp.com"]
 
